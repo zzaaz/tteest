bad9d28a03271502ac501e6ca7ad23e58f3a55c6
zazascode <zzank19@freeuni.edu.ge>
2022-06-04 02:38:32 +0400

    branch1commit

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 name = input("please input your name: ")
 
 print(f"your name is {name}")
+
+age = input("please tell us your age: ")
+
+print(f"your name ia {name} and your age is {age}")
+
+
+#changeees
